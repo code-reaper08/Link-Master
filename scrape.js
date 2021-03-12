de48ea971d7534014a33d91012b4c5236763483b
code-reaper08 <v.is.h.w.a.08.11.2001@gmail.com>
2021-03-12 23:01:17 +0530

    Made to fetch data

--- scrape.js
+++ scrape.js
@@ -1,45 +1,73 @@
-// const request = require('request');
-// const cheerio = require('cheerio');
-
-// request('', (error,response,html) =>{
-//     if(!error && response.statusCode == 200) {
-//         const $ = cheerio.load(html);
-//         const linkObjects = $('a');
-//         const links = [];
-//         linkObjects.each((index, element) => {
-//             links.push({
-//               text: $(element).text(), 
-//               href: $(element).attr('href'),
-//             });
-//           });
-      
-//           console.log(links);
-//     }
-// });
-
-
-
-
 const request = require('request');
 const cheerio = require('cheerio');
 
-request('https://github.com/code-reaper08/Know-ASCII/blob/main/README.md', (error,response,html) =>{
+request('https://github.com/code-reaper08/GameBoy-Adventures/blob/main/README.md', (error,response,html) =>{
     if(!error && response.statusCode == 200) {
         const $ = cheerio.load(html);
-
-        //const readmebox = $('#readme > article');
-        $('#readme > article').each((i,el) => {
-            // const item = $(el).text();
-            const link = $(el).find('a').attr('href');
-            // console.log(item);
-            console.log(link);
-        })
-
-        //console.log(readmebox.find('a').attr('href'));
-        // console.log(reponame.text());
-        // const repofiles = reponame.find('a').attr('href');
-        // console.log(repofiles);
-     
+        const linkObjects = $('#readme > article').find('a');
+        const links = [];
+        linkObjects.each((index, element) => {
+            links.push({
+              text: $(element).text(), 
+              href: $(element).attr('href'),
+            });
+          });
+      
+          console.log(links);
     }
-
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const request = require('request');
+// const cheerio = require('cheerio');
+
+// request('https://github.com/code-reaper08/Know-ASCII/blob/main/README.md', (error,response,html) =>{
+//     if(!error && response.statusCode == 200) {
+//         const $ = cheerio.load(html);
+
+//         //const readmebox = $('#readme > article');
+//         $('#readme > article').each((i,el) => {
+//             const item = $(el).text();
+//             const link = $(el).find('a').attr('href');
+//             console.log(item);
+//             // console.log(link);
+//         })
+
+//         //console.log(readmebox.find('a').attr('href'));
+//         // console.log(reponame.text());
+//         // const repofiles = reponame.find('a').attr('href');
+//         // console.log(repofiles);
+     
+//     }
+
+// });
