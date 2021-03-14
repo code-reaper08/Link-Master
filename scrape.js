fd22f0a0f8cc7b3c2a61b973e16fbb93646798b8
code-reaper08 <v.is.h.w.a.08.11.2001@gmail.com>
2021-03-15 00:16:50 +0530

    Functionality enhanced and white space trimmed

--- scrape.js
+++ scrape.js
@@ -1,87 +1,34 @@
-const request = require('request');
-const cheerio = require('cheerio');
+const request = require("request");
+const cheerio = require("cheerio");
 
-request('https://github.com/code-reaper08/Know-ASCII/blob/main/README.md', (error,response,html) =>{
-    if(!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
-        const linkObjects = $('#readme > article').children().find('a');
-        const links = [];
-        linkObjects.each((index, element) => {
-            links.push({
-              // text: $(element).text(), 
-              href: $(element).attr('href'),
-            });
-          });
-          const len = links.length;
-        //   console.log(len);
-          // console.log(links);
-          // var arrayLength = myStringArray.length;
-          for (let i = 0; i <len ; i++) {
-              // console.log("loop running")
-              const hrefs = links[i].href;
-              request(hrefs,(error,response,html) => {
-                if(!error && response.statusCode == 200) {
-                  console.log(hrefs,' Success');
-                }
-                else if(!error && response.statusCode != 200) {
-                  console.log(hrefs,' Failure');
-                }
-              })
-}
+request(
+  "https://github.com/code-reaper08/MarkdownAssistant/blob/main/README.md",
+  (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+      const $ = cheerio.load(html);
+      const linkObjects = $("#readme > article").children().find("a");
+      const links = [];
+      linkObjects.each((index, element) => {
+        links.push({
+          // text: $(element).text(),
+          href: $(element).attr("href"),
+        });
+      });
+      const len = links.length;
+      //   console.log(len);
+      // console.log(links);
+      // var arrayLength = myStringArray.length;
+      for (let i = 0; i < len; i++) {
+        // console.log("loop running")
+        const hrefs = links[i].href.replace(/\s\s+/g, "");
+        request(hrefs, (error, response, html) => {
+          if (!error && response.statusCode == 200) {
+            console.log(hrefs, " Success");
+          } else if (!error && response.statusCode != 200) {
+            console.log(hrefs, " Failure");
+          }
+        });
+      }
     }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const request = require('request');
-// const cheerio = require('cheerio');
-
-// request('https://github.com/code-reaper08/Know-ASCII/blob/main/README.md', (error,response,html) =>{
-//     if(!error && response.statusCode == 200) {
-//         const $ = cheerio.load(html);
-
-//         //const readmebox = $('#readme > article');
-//         $('#readme > article').each((i,el) => {
-//             const item = $(el).text();
-//             const link = $(el).find('a').attr('href');
-//             console.log(item);
-//             // console.log(link);
-//         })
-
-//         //console.log(readmebox.find('a').attr('href'));
-//         // console.log(reponame.text());
-//         // const repofiles = reponame.find('a').attr('href');
-//         // console.log(repofiles);
-     
-//     }
-
-// });
+  }
+);
