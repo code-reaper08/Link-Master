2cd057cb62e9da244a613a305903cc7ff778c61c
code-reaper08 <v.is.h.w.a.08.11.2001@gmail.com>
2021-03-18 23:56:54 +0530

    front end in developement

--- input.js
+++ input.js
@@ -1,30 +1,30 @@
-// const searchInput = document.getElementById('search-input');
-// const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
 
-// searchButton.addEventListener('click', (e)=>{
-// 	e.preventDefault();
-// 	const userlink = searchInput.value;
-//     sessionStorage.setItem("userlink", userlink);
-//     location.reload();
-//     location.reload();
-    
-// })
-
-
-
-$("#search-button").dblclick(function(e) { 
-    console.log("dbl click simulated ");
-    e.preventDefault();
-    const userlink = $("#search-input").val();
+searchButton.addEventListener('click', (e)=>{
+	e.preventDefault();
+	const userlink = searchInput.value;
     sessionStorage.setItem("userlink", userlink);
     location.reload();
-    // alert('dblclick');
-  });
-  $("#search-button").click(function() { 
-    // alert('click');
-    $(this).dblclick(); 
-  });
-  
-//   $("#container2").click(function() { 
-//     $("#container").click();
+    location.reload();
+    
+})
+
+
+
+// $("#search-button").dblclick(function(e) { 
+//     console.log("dbl click simulated ");
+//     e.preventDefault();
+//     const userlink = $("#search-input").val();
+//     sessionStorage.setItem("userlink", userlink);
+//     location.reload();
+//     // alert('dblclick');
 //   });
+//   $("#search-button").click(function() { 
+//     // alert('click');
+//     $(this).dblclick(); 
+//   });
+  
+// //   $("#container2").click(function() { 
+// //     $("#container").click();
+// //   });
