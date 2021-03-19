20b6076df526d60ba290ca20abe862a951c61690
code-reaper08 <v.is.h.w.a.08.11.2001@gmail.com>
2021-03-19 16:57:32 +0530

    All work done.

--- src/input.js
+++ src/input.js
@@ -2,7 +2,6 @@ const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
 
 
-
 searchButton.addEventListener('click', (e)=>{
 	e.preventDefault();
 	const userlink = searchInput.value;
@@ -11,22 +10,3 @@ searchButton.addEventListener('click', (e)=>{
     location.reload();
     
 })
-
-
-
-// $("#search-button").dblclick(function(e) { 
-//     console.log("dbl click simulated ");
-//     e.preventDefault();
-//     const userlink = $("#search-input").val();
-//     sessionStorage.setItem("userlink", userlink);
-//     location.reload();
-//     // alert('dblclick');
-//   });
-//   $("#search-button").click(function() { 
-//     // alert('click');
-//     $(this).dblclick(); 
-//   });
-  
-// //   $("#container2").click(function() { 
-// //     $("#container").click();
-// //   });
